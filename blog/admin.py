b58cd877da41454db33bd277821ae0454ed17658
Diana <karaevadiana176@gmail.com>
2024-08-21 17:34:59 +0600

    21.08

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.contrib import admin
 from blog.models import Publication, Category, Hashtag,PublicationComment
+
 
 
 @admin.register(Category)
--- super_info_core/settings.py
+++ super_info_core/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os.path
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'super_info_core.settings')
+
+application = get_wsgi_application()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ckeditor',
+    'modeltranslation',
     'blog',
 
 
@@ -46,6 +54,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'modeltranslation.middleware.TranslationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -107,11 +117,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
+LANGUAGES = [
+    ('en', _('English')),
+    ('kg', _('Kyrgyz')),
+    ('ru', _('Russian')),
+]
+
+LOCALE_PATHS = os.path.join(BASE_DIR, 'locale')
+
+
+USE_I18N = True
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Bishkek'
-
-USE_I18N = True
 
 USE_TZ = True
 
